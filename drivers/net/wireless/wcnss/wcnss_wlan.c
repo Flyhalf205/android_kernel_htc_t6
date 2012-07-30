0befb09244bbbf7d64f18badc3fe49cb40836a17
Sameer Thalappil <sameert@codeaurora.org>
2012-07-30 12:56:37 -0700

    wcnss: wake lock APIs for host drivers
    wcnss related host drivers can use these APIs to prevent Apps suspend.
    WLAN driver uses this API to prevent suspend, when it's module
    initialization routines are being executed.
    
    Change-Id: Id072804e6ccc953c78caf6cf11c73e0ea1d71d18
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>
    
    Conflicts:
    	include/linux/wcnss_wlan.h

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -21,6 +21,7 @@
 #include <linux/workqueue.h>
 #include <linux/jiffies.h>
 #include <linux/gpio.h>
+#include <linux/wakelock.h>
 #include <mach/peripheral-loader.h>
 #include <linux/io.h>
 #include <mach/msm_iomap.h>
@@ -53,6 +54,7 @@ static struct {
 	void		(*tm_notify)(struct device *, int);
 	struct wcnss_wlan_config wlan_config;
 	struct delayed_work wcnss_work;
+	struct wake_lock wcnss_wake_lock;
 } *penv = NULL;
 
 static ssize_t wcnss_serial_number_show(struct device *dev,
@@ -65,7 +67,7 @@ static ssize_t wcnss_serial_number_show(struct device *dev,
 }
 
 static ssize_t wcnss_serial_number_store(struct device *dev,
-		struct device_attribute *attr, const char * buf, size_t count)
+		struct device_attribute *attr, const char *buf, size_t count)
 {
 	unsigned int value;
 
@@ -93,7 +95,7 @@ static ssize_t wcnss_thermal_mitigation_show(struct device *dev,
 }
 
 static ssize_t wcnss_thermal_mitigation_store(struct device *dev,
-		struct device_attribute *attr, const char * buf, size_t count)
+		struct device_attribute *attr, const char *buf, size_t count)
 {
 	int value;
 
@@ -338,6 +340,20 @@ static int wcnss_wlan_resume(struct device *dev)
 	return 0;
 }
 
+void wcnss_prevent_suspend()
+{
+	if (penv)
+		wake_lock(&penv->wcnss_wake_lock);
+}
+EXPORT_SYMBOL(wcnss_prevent_suspend);
+
+void wcnss_allow_suspend()
+{
+	if (penv)
+		wake_unlock(&penv->wcnss_wake_lock);
+}
+EXPORT_SYMBOL(wcnss_allow_suspend);
+
 static int
 wcnss_trigger_config(struct platform_device *pdev)
 {
@@ -409,6 +425,8 @@ wcnss_trigger_config(struct platform_device *pdev)
 	ret = wcnss_create_sysfs(&pdev->dev);
 	if (ret)
 		goto fail_sysfs;
+
+	wake_lock_init(&penv->wcnss_wake_lock, WAKE_LOCK_SUSPEND, "wcnss");
 
 	return 0;
 
