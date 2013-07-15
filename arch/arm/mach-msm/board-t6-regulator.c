e2fcc46442d94553b52969c1e6fca787cf7de1e0
Paul Reioux <reioux@gmail.com>
2013-07-15 15:55:26 -0500

    CPU Overclock: initial overclock for Krait 600 (1.7 GHz variants)
    overclocking up to 2.1 GHz
    
    Signed-off-by: Paul Reioux <faux123@localhost.localdomain>

--- arch/arm/mach-msm/board-t6-regulator.c
+++ arch/arm/mach-msm/board-t6-regulator.c
@@ -510,15 +510,31 @@ t6_gpio_regulator_pdata[] __devinitdata = {
 /* SAW regulator constraints */
 struct regulator_init_data t6_saw_regulator_pdata_8921_s5 =
 	/*	      ID  vreg_name	       min_uV   max_uV */
+#ifdef CONFIG_CPU_OVERCLOCK
+	SAW_VREG_INIT(S5, "8921_s5",	       850000, 1400000);
+#else
 	SAW_VREG_INIT(S5, "8921_s5",	       850000, 1300000);
+#endif
 struct regulator_init_data t6_saw_regulator_pdata_8921_s6 =
+#ifdef CONFIG_CPU_OVERCLOCK
+	SAW_VREG_INIT(S5, "8921_s6",	       850000, 1400000);
+#else
 	SAW_VREG_INIT(S6, "8921_s6",	       850000, 1300000);
+#endif
 
 struct regulator_init_data t6_saw_regulator_pdata_8821_s0 =
 	/*	      ID       vreg_name	min_uV  max_uV */
+#ifdef CONFIG_CPU_OVERCLOCK
+	SAW_VREG_INIT(8821_S0, "8821_s0",       850000, 1400000);
+#else
 	SAW_VREG_INIT(8821_S0, "8821_s0",       850000, 1300000);
+#endif
 struct regulator_init_data t6_saw_regulator_pdata_8821_s1 =
+#ifdef CONFIG_CPU_OVERCLOCK
+	SAW_VREG_INIT(8821_S0, "8821_s1",       850000, 1400000);
+#else
 	SAW_VREG_INIT(8821_S1, "8821_s1",       850000, 1300000);
+#endif
 
 /* PM8921 regulator constraints */
 struct pm8xxx_regulator_platform_data
