6ce945aba690c41792d084938681bcaef9f09889
Bryan Maltais <brymaster5000@gmail.com>
2014-04-18 22:43:17 -0400

    CPU Overclock up to 2.1 GHz
    Conflicts:
    	arch/arm/configs/t6wl_defconfig
    	arch/arm/mach-msm/Kconfig
    
    Change-Id: I807d2c6d261195109bdee6e942ba39981ff68049

--- arch/arm/mach-msm/board-t6wl.c
+++ arch/arm/mach-msm/board-t6wl.c
@@ -3355,7 +3355,11 @@ static struct platform_device msm_tsens_device = {
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 1000,
+#ifdef CONFIG_CPU_OVERCLOCK
+	.limit_temp = 75,
+#else
 	.limit_temp = 50,
+#endif
 	.temp_hysteresis = 5,
 	.limit_freq = 918000,
 };
